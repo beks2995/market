7813eab8a999f85ba197a0b28d350ff02aa761c3
Miki-2006 <rys5ek0v.111@gmail.com>
2024-05-15 20:02:38 +0600

    Типизация карточки и его структура

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { db } from "../../firebase/firestore";
 import { useEffect } from "react";
 import { collection, query, onSnapshot } from "firebase/firestore";
 import './Homepage.css'
 
+interface Idata {
+    category: string;
+    description: string;
+    estimation: string;
+    img: string;
+    inBasket: boolean;
+    inFavorite: boolean;
+    name: string;
+    priceAfterDiscount: string;
+    priceBeforeDiscount: string;
+}
+
 const Home: React.FC  = () => {
+    const [data, setData] = useState<Array<Idata>>([])
     useEffect (() => {
         const q = query(collection(db, "products/product_id/headphones"));
         const unsubscribe = onSnapshot (q, (querySnapshot) => {
@@ -13,6 +26,7 @@ const Home: React.FC  = () => {
                 arr.push(doc.data());
             });
             console.log(arr);
+            setData(arr)
         });
         return () => unsubscribe()
         
@@ -30,6 +44,30 @@ const Home: React.FC  = () => {
                     </div>
                     {/* <img src="../../../images/iPhone-13-Pro-Max-silver-1000x1000 1.jpg" alt="phone" /> */}
                 </div>
+            </section>
+
+            <section>
+                {
+                    data.map(el => (
+                        <div className="headphones">
+                            <div className="headphones__card">
+                                <img src="./images/Vector (2).svg" alt="favorite" />
+                                <img src={el.img} alt="" />
+                                <div className="headphones__info">
+                                    <b className="headphones__info-title">{el.name}</b>
+                                    <div className="headphones__info-price">
+                                        <p>{el.priceAfterDiscount}</p>
+                                        <p>{el.priceBeforeDiscount}</p>
+                                    </div>
+                                </div>
+                                <div className="headphones__estimation">
+                                    <img src="./images/Vector (3).svg" alt="estimation" />
+                                    <p>{el.estimation}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </section>
         </main>
     )
