5f18233b9cd76579cb9e65d6ade180d8af7cd6ed
maksat <maksatsat1123@gmail.com>
2024-05-18 12:00:03 +0300

    условия сервиса (без header)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-
-function App() {
+import React from 'react'
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
+import UslovieService from './pages/UslovieService/UslovieService'
+import Contacts from './pages/Contacts/Contacts'
+const App = () => {
   return (
-    <div className="App">
-     
+    <div>
+      <Routes>
+        <Route path='/' element={<UslovieService />}></Route>
+        <Route path='/Contacts' element={<Contacts />}></Route>
+      </Routes>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
