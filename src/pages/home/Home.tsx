8d178a4793d49d2aea61c51867db053e9c0b9744
Bek <beks2992@gmail.com>
2024-05-18 19:49:54 +0600

    fix home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,21 +5,6 @@ import Headphones from "../../components/headphones";
 
 const Home: React.FC  = () => {
 
-    const [data, setData] = useState<Array<Idata>>([])
-    useEffect (() => {
-        const q = query(collection(db, "products/product_id/Headphones"));
-        const unsubscribe = onSnapshot (q, (querySnapshot) => {
-            const arr:any = [];
-            querySnapshot.forEach((doc) => {
-                arr.push(doc.data());
-            });
-            console.log(arr);
-            setData(arr)
-        });
-        return () => unsubscribe()
-        
-    }, []) 
-
     return (
         <main>
             <Top/>
