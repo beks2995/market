e03f1d6617060c167e2888eb86a0a93a7ece059c
Miki-2006 <rys5ek0v.111@gmail.com>
2024-05-23 13:24:26 +0600

    слияние с веткой develop

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,22 +4,6 @@ import Top from "./sections/top";
 import Headphones from "../../components/headphones";
 
 const Home: React.FC  = () => {
-
-    const [data, setData] = useState<Array<Idata>>([])
-    useEffect (() => {
-        const q = query(collection(db, "products/product_id/Headphones"));
-        const unsubscribe = onSnapshot (q, (querySnapshot) => {
-            const arr:any = [];
-            querySnapshot.forEach((doc) => {
-                arr.push(doc.data());
-            });
-            console.log(arr);
-            setData(arr)
-        });
-        return () => unsubscribe()
-        
-    }, []) 
-
     return (
         <main>
             <Top/>
