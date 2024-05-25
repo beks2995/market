318f78185609ba1a449793b75660b29e17dd22c6
blusnake <sunkaralisir@gmail.com>
2024-05-25 17:53:11 +0600

    final commit before saturday show of project added adaptation according to screen size

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import {FC, useState} from 'react';
+import {FC, useState, useEffect} from 'react';
 import Header from './components/Header/Header'; // Importing the Header component
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login'
@@ -9,13 +9,30 @@ import ProductPage from './pages/Product/ProductPage';
 import UslovieService from './pages/UslovieService/UslovieService'
 import Contacts from './pages/Contacts/Contacts'
 import Footer from './components/Footer/Footer'
+import { useWindowSize } from 'react-use'; // Assuming you use a library for window size
 
 const App: FC = () => {
   const [favoritedCount, setFavoritedCount] = useState(0);
+  const { width } = useWindowSize(); // Get window width
+  const [isMobile, setIsMobile] = useState<boolean>(width < 429);
+  useEffect(() => {
+    const handleResize = () => {
+        setIsMobile(window.innerWidth < 429);
+        // console.log('isMobile:', isMobile)
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove event listener
+    return () => {
+        window.removeEventListener('resize', handleResize);
+    };
+});
+  
   return (
     <Router>
       <div className='App'>
-        <Header  favoritedCount={favoritedCount}/> {/* Adding the Header to the App component */}
+        <Header  favoritedCount={favoritedCount} isMobile={isMobile}/> {/* Adding the Header to the App component */}
         <main className='Content'>
         <Routes>
           <Route path="/" element={<Home setFavoritedCount={setFavoritedCount}/>} />
@@ -32,7 +49,7 @@ const App: FC = () => {
           <Route path="/register" element={<Register />} />
         </Routes>
         </main>
-        <Footer />
+        <Footer isMobile={isMobile}/>
       </div>
     </Router>
   );
