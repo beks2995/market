d60c0e8f04d426af89e0fa2c4e024634d23f6d77
Adelya <msvadelya@gmail.com>
2024-05-25 15:48:33 +0600

    hid adding review function from unauthorized users

--- src/components/Product/CommentSection.tsx
+++ src/components/Product/CommentSection.tsx
@@ -1,27 +1,41 @@
 import UserComment from "./UserComment";
 import CommentTextArea from "./CommentTextArea";
 import { useEffect, useState } from "react";
-import {Comment} from "./types"
-import {Review, User} from "../../types/types"
+import { Comment } from "./types";
+import { Review, User } from "../../types/types";
 import useAuth from "../../hooks/useAuth";
 
 type CommentSectionProps = {
   reviews: Review[];
-  user:User;
+  user: User;
 };
 
-function CommentSection({reviews}:CommentSectionProps) {
-  const [comments, setComments] = useState<Review[] | null>(reviews);
-  const user=useAuth();
+function CommentSection({ reviews }: CommentSectionProps) {
+  const [comments, setComments] = useState<Review[] | null>(
+    [...reviews].reverse()
+  );
+  const user = useAuth();
 
   return (
     <div>
       <div className="mb-20 space-y-6">
         {comments?.map((c) => (
-          <UserComment key={c.id} username={c.username} comment={c.comment} rating={c.rating}/>
+          <UserComment
+            key={c.id}
+            username={c.username}
+            comment={c.comment}
+            rating={c.rating}
+          />
         ))}
       </div>
-      <CommentTextArea setComments={setComments} username={user?.displayName??"User"}/>
+      {user ? (
+        <CommentTextArea
+          setComments={setComments}
+          username={user?.displayName ?? "User"}
+        />
+      ) : (
+        <div>Отзыв мошут оставлять только зарегистрированные пользователи</div>
+      )}
     </div>
   );
 }
