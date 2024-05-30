5f4f0e46fdfdb2ff4336a7c7db592630feb2be2a
Miki-2006 <rys5ek0v.111@gmail.com>
2024-05-30 16:15:21 +0600

    Изменения в стилях

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,19 +1,29 @@
-import { FC, SetStateAction, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Idata } from "../home/interfaces";
 import Card from "../../components/Card";
+import './Favorites.css'
 
 const Favorites:FC = () => {
-    const [favoriteProducts, setFavoriteProducts] = useState<SetStateAction<Idata[]>>([])
+    const [favoriteProducts, setFavoriteProducts] = useState<Idata[]>([])
     useEffect(() => {
         setFavoriteProducts(JSON.parse(localStorage.getItem('inFavorited') as any));
         
     }, [])
     console.log(favoriteProducts);
     
+    const clickHandle = (el: Idata) => {
+        el.isFavorited = !el.isFavorited
+        setFavoriteProducts((prev: Idata[]) => prev.filter(el => el.isFavorited ? el : ''))
+        if(el.isFavorited){
+            setFavoriteProducts((prev: Idata[]) => [...prev, el])
+        }
+        
+    } 
 
     return (
-        <div>
-            {/* <Card /> */}
+        <div className="favorite_products">
+            <h2 className="favorite_products_title">Избранные</h2>
+            <Card data={favoriteProducts} clickHandle={clickHandle} inFavorited={favoriteProducts}/>
         </div>
     )
 }
