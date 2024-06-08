57591d0072cdaa364ba88de3b428c7a9e3560092
Bek <beks2992@gmail.com>
2024-06-08 19:14:58 +0600

    fix root router

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"; // Importing the App component
 import "./App.css";
+import { BrowserRouter as Router} from 'react-router-dom';
+
 
 ReactDOM.render(
-  <div>
+  <Router>
     <App /> {/* Rendering the App component */}
-  </div>,
+  </Router>,
   document.getElementById("root") // Make sure there's a corresponding div with id="root" in your HTML
 );
