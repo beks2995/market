eb98765c6d4ac0eea5ddab11132f6615696a4d69
rthtc <osmonalievadilet2007@gmail.com>
2024-06-13 12:22:10 +0600

    стилизация заказа

--- src/components/Basket/CheckoutPage/CheckoutPage.tsx
+++ src/components/Basket/CheckoutPage/CheckoutPage.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { db } from '../../../firebase/firestore';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
+import './CheckoutPage.css'
 
 const Checkout: React.FC = () => {
+    const mapRef = useRef<HTMLDivElement | null>(null);
     const location = useLocation();
     const navigate = useNavigate();
     const { cartItems, deliveryOption } = location.state || {}
@@ -55,6 +57,21 @@ const Checkout: React.FC = () => {
         fetchItemsData()
     }, [cartItems])
 
+    useEffect(() => {
+        if (mapRef.current && window.google) {
+            const map = new window.google.maps.Map(mapRef.current, {
+                center: { lat: 42.88026059336971, lng: 74.58884497026457 },
+                zoom: 12,
+            });
+
+            new window.google.maps.Marker({
+                position: { lat: 42.88026059336971, lng: 74.58884497026457 },
+                map: map,
+                title: "Метка",
+            });
+        }
+    }, []);
+
     const finalTotalCost = totalCost + (deliveryOption === 'courier' ? 499 : 0)
 
     const validateForm = () => {
@@ -90,7 +107,7 @@ const Checkout: React.FC = () => {
             deliveryAddress,
             paymentStatus: 'pending',
             createdAt: new Date(),
-            orderNumber 
+            orderNumber
         }
 
         try {
@@ -103,85 +120,140 @@ const Checkout: React.FC = () => {
     };
 
     return (
-        <div>
-            <h2>Оформление заказа</h2>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <div style={{ width: '48%' }}>
-                    <h3>Доставка курьером</h3>
-                    <p>Адрес доставки</p>
-                    <input
-                        type="text"
-                        placeholder="Город"
-                        value={deliveryAddress.city}
-                        onChange={e => setDeliveryAddress({ ...deliveryAddress, city: e.target.value })}
-                    />
-                    {errors.city && <p style={{ color: 'red' }}>{errors.city}</p>}
-                    <input
-                        type="text"
-                        placeholder="Улица / Район"
-                        value={deliveryAddress.street}
-                        onChange={e => setDeliveryAddress({ ...deliveryAddress, street: e.target.value })}
-                    />
-                    {errors.street && <p style={{ color: 'red' }}>{errors.street}</p>}
-                    <input
-                        type="text"
-                        placeholder="Дом"
-                        value={deliveryAddress.house}
-                        onChange={e => setDeliveryAddress({ ...deliveryAddress, house: e.target.value })}
-                    />
-                    {errors.house && <p style={{ color: 'red' }}>{errors.house}</p>}
-                    <input
-                        type="text"
-                        placeholder="Подъезд"
-                        value={deliveryAddress.entrance}
-                        onChange={e => setDeliveryAddress({ ...deliveryAddress, entrance: e.target.value })}
-                    />
-                    {errors.entrance && <p style={{ color: 'red' }}>{errors.entrance}</p>}
-                    <input
-                        type="text"
-                        placeholder="Квартира"
-                        value={deliveryAddress.apartment}
-                        onChange={e => setDeliveryAddress({ ...deliveryAddress, apartment: e.target.value })}
-                    />
-                    {errors.apartment && <p style={{ color: 'red' }}>{errors.apartment}</p>}
-                </div>
-                <div style={{ width: '48%' }}>
-                    <h3>Ваш заказ</h3>
-                    {itemsData.map((item, index) => (
-                        <div key={index}>
-                            <p> {item.quantity}х {item.name}   {item.totalCost} сом</p>
-                        </div>
-                    ))}
-                    <p>{deliveryOption === 'courier' ? 'Доставка  499 сом' : 'Самовывоз - бесплатно'}</p>
-                    <p>К оплате {finalTotalCost} сом</p>             
-                    <h3>Способ оплаты</h3>
-                    <div>
-                        <button
-                            className="mt-4 px-4 py-2 "
-                            onClick={() => setShowQRCode(!showQRCode)}
-                        >
-                            Оплата на mBank
-                        </button>
-                        {showQRCode && (
-                            <div style={{ marginTop: '20px', textAlign: 'center' }}>
-                                <h3>Отсканируйте QR код для оплаты</h3>
-                                <img src="https://i.postimg.cc/LskL8FFD/qr-code.gif" alt="QR Code" />
+        <div className='order'>
+            <h2 className='order__title'>Оформление заказа</h2>
+            <div className='order__container'>
+                <div className='order__decoration'>
+                    <div className="order__decoration-container">
+                        <div className="order__decoration-block">
+                            <div className="order__title-block">
+                                <h3 className='order__decoration-title'>Доставка курьером</h3>
+                                <span className="order__delivery">499 сом</span>
                             </div>
-                        )}
+                            <div ref={mapRef} className="order__decoration-map"></div>
+                            <p className='order__decoration-description'><img src="https://i.postimg.cc/1zS2Ht9k/Vector-2.png" alt="location" />Адрес доставки</p>
+                            <div className="order__decoration-container-inputs">
+                                <div className="order__decoration-container-input">
+                                    <div className="order__decoration-input">
+                                        <input
+                                            className='order__decoration-city'
+                                            type="text"
+                                            placeholder="Город"
+                                            value={deliveryAddress.city}
+                                            onChange={e => setDeliveryAddress({ ...deliveryAddress, city: e.target.value })}
+                                        />
+                                        <span className='order__decoration-span'>
+                                            <img src="https://i.postimg.cc/sg0dVyyq/Vector-3.png" alt="arrow" className="order__decoration-img" />
+                                        </span>
+                                        {errors.city && <p style={{ color: 'red' }}>{errors.city}</p>}
+                                    </div>
+                                    <div className="order__decoration-input">
+                                        <input
+                                            className='order__decoration-city'
+                                            type="text"
+                                            placeholder="Улица / Район"
+                                            value={deliveryAddress.street}
+                                            onChange={e => setDeliveryAddress({ ...deliveryAddress, street: e.target.value })}
+                                        />
+                                        <span className='order__decoration-span'>
+                                            <img src="https://i.postimg.cc/mDfGf2bS/Vector-4.png" alt="arrow" className="order__decoration-img" />
+                                        </span>
+                                        {errors.street && <p style={{ color: 'red' }}>{errors.street}</p>}
+                                    </div>
+                                </div>
+                                <div className="order__decoration-home-block">
+                                    <div className="order__decoration-home-block-first">
+                                        <div className="order__decoration-home-input">
+                                            <input
+                                                className="order__decoration-home"
+                                                type="text"
+                                                placeholder="Дом"
+                                                value={deliveryAddress.house}
+                                                onChange={e => setDeliveryAddress({ ...deliveryAddress, house: e.target.value })}
+                                            />
+                                            <span className="order__decoration-home-span">
+                                                <img src="https://i.postimg.cc/mDfGf2bS/Vector-4.png" alt="arrow" className="order__decoration-home-img" />
+                                            </span>
+                                            {errors.house && <p style={{ color: 'red' }}>{errors.house}</p>}
+                                        </div>
+                                        <div className="order__decoration-home-input">
+                                            <input
+                                                className="order__decoration-home"
+                                                type="text"
+                                                placeholder="Квартира"
+                                                value={deliveryAddress.apartment}
+                                                onChange={e => setDeliveryAddress({ ...deliveryAddress, apartment: e.target.value })}
+                                            />
+                                            <span className="order__decoration-home-span">
+                                                <img src="https://i.postimg.cc/mDfGf2bS/Vector-4.png" alt="arrow" className="order__decoration-home-img" />
+                                            </span>
+                                            {errors.apartment && <p style={{ color: 'red' }}>{errors.apartment}</p>}
+                                        </div>
+                                    </div>
+                                    <div className="order__decoration-home-input">
+                                        <input
+                                            className="order__decoration-home"
+                                            type="text"
+                                            placeholder="Подъезд"
+                                            value={deliveryAddress.entrance}
+                                            onChange={e => setDeliveryAddress({ ...deliveryAddress, entrance: e.target.value })}
+                                        />
+                                        <span className="order__decoration-home-span-p">
+                                            <img src="https://i.postimg.cc/mDfGf2bS/Vector-4.png" alt="arrow" className="order__decoration-home-img" />
+                                        </span>
+                                        {errors.entrance && <p style={{ color: 'red' }}>{errors.entrance}</p>}
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <input
-                        type="text"
-                        placeholder="Номер получателя"
-                        value={customerPhone}
-                        onChange={e => setCustomerPhone(e.target.value)}
-                    />
-                    {errors.customerPhone && <p style={{ color: 'red' }}>{errors.customerPhone}</p>}
-                    <button
-                        className="mt-4 px-4 py-2  "
-                        onClick={handleOrderSubmit}
-                    >
-                        Закончить оформление
-                    </button>
+                    <div className='order__info'>
+                        <div className="order__info-container">
+                            <div className="order__info-your">
+                                <h3 className='order__info-your-title'>Ваш заказ</h3>
+                                <div className="order__info-your-product-card">
+                                    {itemsData.map((item, index) => (
+                                        <div className='order__info-your-product' key={index}>
+                                            <p className='order__info-your-text-block'> <span className='order-info-your-text'>{item.quantity}х</span> {item.name}</p> <p className='order-info-your-text'>{item.totalCost} сом</p>
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="order__info-your-total">
+                                    <p className='order__info-your-delivery'>{deliveryOption === 'courier' ? 'Доставка' : 'Самовывоз - бесплатно'} <span className='order__info-your-span'>499 сом</span></p>
+                                    <p className='order__info-your-delivery'>К оплате<span> {finalTotalCost} сом</span></p>
+                                </div>
+                            </div>
+                            <h3>Способ оплаты</h3>
+                            <div>
+                                <button
+                                    className="mt-4 px-4 py-2 "
+                                    onClick={() => setShowQRCode(!showQRCode)}
+                                >
+                                    Оплата на mBank
+                                </button>
+                                {showQRCode && (
+                                    <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                                        <h3>Отсканируйте QR код для оплаты</h3>
+                                        <img src="https://i.postimg.cc/LskL8FFD/qr-code.gif" alt="QR Code" />
+                                    </div>
+                                )}
+                            </div>
+                            <input
+                                type="text"
+                                placeholder="Номер получателя"
+                                value={customerPhone}
+                                onChange={e => setCustomerPhone(e.target.value)}
+                            />
+                            {errors.customerPhone && <p style={{ color: 'red' }}>{errors.customerPhone}</p>}
+                            <button
+                                className="mt-4 px-4 py-2  "
+                                onClick={handleOrderSubmit}
+                            >
+                                Закончить оформление
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
