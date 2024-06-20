de0d07299199e5ab0a18a6794d80354e42d36909
Miki-2006 <rys5ek0v.111@gmail.com>
2024-06-20 19:08:23 +0600

    Fixing icon of favorite products

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -23,23 +23,14 @@ const Card = ({el, setInFavorited, inFavorited}: IProps) => {
     }
     useEffect(() => {
         localStorage.setItem("inFavorited", JSON.stringify(inFavorited))
-    }, [inFavorited])
-    const clickIconHandle = ((products:Array<Idata>, item: Idata) => {
-        // const finded = 
-        products.some((el) => item.description === el.description)
-        // if(finded){
-        //     item.isFavorited
-        // } else el.isFavorited
-    })
-    
+    }, [inFavorited])    
     
     return (
         <div>                                                                                                                   
             <div className="card__top">
                 <div onClick={(e: MouseEvent<HTMLDivElement>) => clickHandle && clickHandle(el)}>
                     {
-                        // inFavorited.length > 0 ?  () => clickIconHandle(inFavorited, el) :
-                         el.isFavorited
+                        inFavorited && inFavorited.find((item: Idata) => item.name === el.name)?.isFavorited 
                         ?
                         <svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.001 1.88189C12.35 -0.227115 15.98 -0.157115 18.243 2.10989C20.505 4.37788 20.583 7.98989 18.479 10.3459L9.99901 18.8379L1.52101 10.3459C-0.582994 7.98989 -0.503994 4.37189 1.75701 2.10989C4.02201 -0.154115 7.64501 -0.230115 10.001 1.88189Z" fill="#1C1C27"/>
