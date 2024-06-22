6a21f364b89999c7f61846a209042b12326d195f
maksat <maksatsat1123@gmail.com>
2024-06-22 16:30:22 +0300

    адаптив

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { YMaps, Map, Placemark, GeolocationControl, ZoomControl } from 'react-yandex-maps'
-
+import { YMaps, Map, Placemark, GeolocationControl, ZoomControl } from '@pbe/react-yandex-maps'
+import { Link } from 'react-router-dom'
+import './Contacts.css'
 const Contacts = () => {
   return (
     <div>
@@ -8,36 +8,40 @@ const Contacts = () => {
       <div className='Contacts_top'>
         <div className='map-box'>
           <h3 className='contacts_title'>Наш офис</h3>
-          <YMaps apiKey='3b0cf7b3-13d7-418b-a62a-3be18ff25c06'>
+          <YMaps>
             <div className='map'>
-              <Map  defaultState={{ center: [43.233413, 76.825989], zoom: 18 }} width={722} height={424}>
-  
-         
+              <Map defaultState={{ center: [43.233413, 76.825989], zoom: 18 }} width={window.innerWidth <= 428 ? 345 : 722} height={window.innerWidth<=428? 177:424}>
+
+
                 <Placemark geometry={[43.233413, 76.825989]} properties={{ balloonContent: 'Аксай-3а, 62ф, Алматы, Казахстан' }} />
-                <GeolocationControl options={{ float: 'right', position:{top:'200px',right:'10px'} }} />
+                <GeolocationControl options={{ float: 'right', position: { top: '200px', right: '10px' } }} />
               </Map>
             </div>
           </YMaps>
-          <div style={{ display: 'flex', alignItems: 'center', gap:'11px'}}>
-        <img src="/imgs/address.png" alt="" />
-        <div  >
-            <p className='contacts_address'>Аксай-3а, 62ф, Алматы, Казахстан</p>
-            <p className='address_floor'>3 этаж 35 кабинет</p>
+          <div className='ContactsAddressDiv'>
+            <img src="/imgs/address.png" alt="" />
+            <div>
+              <p className='contacts_address'>Аксай-3а, 62ф, Алматы, Казахстан</p>
+              <p className='address_floor'>3 этаж 35 кабинет</p>
             </div>
           </div>
         </div>
-        <nav>
-          <ul>
-            <li><a href="#"><img src='/imgs/Whatsapp.png' alt="" /></a></li>
-            <li><a href="#"><img src="/imgs/VK.png" alt="" /></a></li>
-            <li><a href="#"><img src="imgs/Instagram.png" alt="" /></a></li>
-            <li><a href="#"><img src='/imgs/Telegram.png' alt="" /></a></li>
-          </ul>
-        </nav>
+
       </div>
-      <div style={{display:'flex',gap:'28px'}} className='Contacts_number'>
+      <nav className='Contacts_nav'>
+        <ul className='Contacts_ul'>
+          <li><a href="#"><img src='/imgs/Whatsapp.png' alt="whatsapp" /></a></li>
+          <li><a href="#"><img src="/imgs/VK.png" alt="vk" /></a></li>
+          <li><a href="#"><img src="imgs/Instagram.png" alt="instaram" /></a></li>
+          <li><a href="#"><img src='/imgs/Telegram.png' alt="telegram" /></a></li>
+        </ul>
+      </nav>
+      <div className='Contacts_number'>
         <img src="/imgs/phone.svg" alt="" />
         <h4>+7 777 777 77 77</h4>
+      </div>
+      <div className='ContactsButtonContainer'>
+      <Link to='/'><button className='ContactsButtonToMain'>На главную</button></Link>
       </div>
     </div>
   )
